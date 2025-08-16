b917e22cf582cc00381ec929bd808a0002f582db
Amrit Manash <amritmanash9@gmail.com>
2025-08-17 00:11:07 +0530

    commiting the file for datatype_demo.py

---
+++ Basics/01_datatypes_demo.py
@@ -0,0 +1,14 @@
+id=12 #Integer data type
+name="Amrit" #String data type
+salary=6000.50 #float data type
+is_skilled=True #Boolean data type as True or False
+
+print("My name is ",name)
+print("My id is ",id)
+print("My salary is Rs.",salary)
+print("I am currently upskilling myself: ",is_skilled)
+
+#we can also verify the data type of variables by
+#using type()
+print("Type of name is",type(name))
+
